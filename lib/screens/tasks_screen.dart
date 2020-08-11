7eab94f631581046300e5db03788786abba4f78e
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-08-11 16:24:01 -0400

    Setting up tasks_screen

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
 
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
