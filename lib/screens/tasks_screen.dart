7521d678845141eddf1935e465c90dc2e327b3e3
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-08-17 16:10:37 -0400

    Beggining layout

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -9,11 +9,44 @@ class TasksScreen extends StatelessWidget {
           padding: 
             EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Icon(Icons.list),
-              Text('Todoey'),
-            ],
-          ),
-        ));
+              CircleAvatar(
+                child: Icon(
+                  Icons.list, 
+                  size: 30.0,
+                  color: Colors.lightBlueAccent,
+                ),
+                backgroundColor: Colors.white,
+                radius: 30.0,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text('Todoey',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+              ),
+              Text(
+                '12 Tasks',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Colors
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
