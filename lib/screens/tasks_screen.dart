352ff0256f254c76bcf12895e31b1ab0574405f5
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-08-18 16:46:27 -0400

    Building ListView

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,47 +5,68 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.lightBlueAccent,
-        body: Container(
-          padding: 
-            EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              CircleAvatar(
-                child: Icon(
-                  Icons.list, 
-                  size: 30.0,
-                  color: Colors.lightBlueAccent,
-                ),
-                backgroundColor: Colors.white,
-                radius: 30.0,
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text('Todoey',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700,
-              ),
-              ),
-              Text(
-                '12 Tasks',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              height: 300.0,
-              decoration: BoxDecoration(
-                color: Colors
-              ),
-            ),
-          ],
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.lightBlueAccent,
+          child: Icon(Icons.add),
         ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: 
+                EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.list, 
+                      size: 30.0,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    backgroundColor: Colors.white,
+                    radius: 30.0,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text('Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  ),
+                  Text(
+                    '12 Tasks',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0))
+              ),
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    title: Text('Feed the cats.'),
+                    trailing: Checkbox(
+                      value: false,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       )
     );
   }
