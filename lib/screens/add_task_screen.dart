b9e43a87405fbb2483b5ff2bb6bfb39026553024
Justin Bartholomew <jbartholomew@turinogroup.com>
2020-08-21 16:04:12 -0400

    Setting up state in task tiles

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -5,12 +5,50 @@ class AddTaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xff757575),
-      child: Container( 
+      child: Container(
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(20.0),
             topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.lightBlueAccent,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+              ),
+              FlatButton(
+                color: Colors.lightBlueAccent,
+                //padding: EdgeInsets.all(40.0),
+                onPressed: () {
+
+                },
+                child: Text(
+                  'Add',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.white,  
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -5,10 +5,27 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('Feed the cats.'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
-      ),
+      trailing: new TaskCheckBox(),
+    );
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  @override
+  _TaskCheckBoxState createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: true,
+      onChanged: (newValue) {
+        isChecked = newValue;
+      },
     );
   }
 }
